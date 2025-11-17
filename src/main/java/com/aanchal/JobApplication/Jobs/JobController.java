80d4467490d79eabb4f81408a149206566decd5e
Aanchal1110 <aanchalupadhyay1009@gmail.com>
2025-11-17 21:52:57 +0530

    Added the JobController

--- src/main/java/com/aanchal/JobApplication/Jobs/JobController.java
+++ src/main/java/com/aanchal/JobApplication/Jobs/JobController.java
@@ -1,11 +1,14 @@
 package com.aanchal.JobApplication.Jobs;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class JobController {
 
     private List<Jobs> jobs=new ArrayList<>();
+    @GetMapping("/jobs")
     public List<Jobs> findAll(){
         return jobs;
     }
