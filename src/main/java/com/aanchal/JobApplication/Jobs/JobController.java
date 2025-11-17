5afef2350caaf3a49aa162f19dad068f8173d819
Aanchal1110 <aanchalupadhyay1009@gmail.com>
2025-11-17 22:37:43 +0530

    Added the create using PostMapping

--- src/main/java/com/aanchal/JobApplication/Jobs/JobController.java
+++ src/main/java/com/aanchal/JobApplication/Jobs/JobController.java
@@ -1,15 +1,26 @@
 package com.aanchal.JobApplication.Jobs;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 public class JobController {
 
     private List<Jobs> jobs=new ArrayList<>();
     @GetMapping("/jobs")
     public List<Jobs> findAll(){
         return jobs;
+    }
+
+    @PostMapping("/jobs")
+    public String createJob(@RequestBody Jobs job){
+        jobs.add(job);
+        return "Job Added Successfully";
+
     }
 }
